99bd71dc32cc1a360a074d3920311791f06fc960
Sambit <skrsambit@gmail.com>
2023-10-06 14:58:24 +0530

    checkbox added for tasks

--- To_Do_List/lib/home_page.dart
+++ To_Do_List/lib/home_page.dart
@@ -9,6 +9,21 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // list of to-do tasks
+  List toDoList = [
+    ["Make Tutorial", false],
+    ["Read a book", false],
+    ["Do Exercise", false],
+    ["Go for a walk", false],
+  ];
+
+  // check box was tapped
+  void checkBoxChanged(bool? value, int index) {
+    setState(() {
+      toDoList[index][1] = !toDoList[index][1];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,10 +32,37 @@ class _HomePageState extends State<HomePage> {
         title: Text('To Do'),
         elevation: 0,
       ),
-      body: ListView(
-        children: [
-          ToDoTile(),
-        ],
+      body: ListView.builder(
+        itemCount: toDoList.length,
+        itemBuilder: (context, index) {
+          return ToDoTile(
+            taskName: toDoList[index][0],
+            taskCompleted: toDoList[index][1],
+            onChanged: (value) => checkBoxChanged(value, index),
+          );
+        },
+        // children: [
+        //   ToDoTile(
+        //     taskName: "Make tutorial",
+        //     taskCompleted: true,
+        //     onChanged: (p0) {},
+        //   ),
+        //   ToDoTile(
+        //     taskName: "Go for a walk",
+        //     taskCompleted: true,
+        //     onChanged: (p0) {},
+        //   ),
+        //   ToDoTile(
+        //     taskName: "Read a book",
+        //     taskCompleted: true,
+        //     onChanged: (p0) {},
+        //   ),
+        //   ToDoTile(
+        //     taskName: "Meditate",
+        //     taskCompleted: false,
+        //     onChanged: (p0) {},
+        //   ),
+        // ],
       ),
     );
   }
--- To_Do_List/lib/util/Todo_tiles.dart
+++ To_Do_List/lib/util/Todo_tiles.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
 
 class ToDoTile extends StatelessWidget {
-  const ToDoTile({super.key});
+  final String taskName;
+  final bool taskCompleted;
+  Function(bool?)? onChanged;
+
+  ToDoTile({
+    super.key,
+    required this.taskName,
+    required this.taskCompleted,
+    required this.onChanged,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        padding: EdgeInsets.all(24),
+        child: Row(
+          children: [
+            //checkbox
+            Checkbox(value: taskCompleted, onChanged: onChanged),
+
+            Text(taskName),
+          ],
+        ),
+        decoration: BoxDecoration(
+          color: Colors.grey,
+          borderRadius: BorderRadius.circular(12),
+        ),
+      ),
+    );
   }
 }
