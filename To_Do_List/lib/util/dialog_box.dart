c9f76ae596235cb0ad313f47a6382c21bf88b97c
Sambit <skrsambit@gmail.com>
2023-10-07 12:10:11 +0530

    buttons created in alert box

--- To_Do_List/lib/util/dialog_box.dart
+++ To_Do_List/lib/util/dialog_box.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_list/util/my_button.dart';
 
 class DialogBox extends StatelessWidget {
   const DialogBox({super.key});
@@ -6,9 +7,44 @@ class DialogBox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      backgroundColor: Colors.grey,
+      backgroundColor: Colors.grey[300],
       content: Container(
         height: 120,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            // get user inout
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                hintText: "Add a new Task",
+              ),
+            ),
+
+            // buttons -> save + cancel
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                // save button
+                MyButton(
+                  text: "Save",
+                  onPressed: () {},
+                ),
+                const SizedBox(
+                  width: 9,
+                ),
+
+                // cancel button
+                MyButton(
+                  text: "Cancel",
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
